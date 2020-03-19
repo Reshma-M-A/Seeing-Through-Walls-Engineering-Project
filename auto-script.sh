4834ed8d22f5e81b5c0b5bb263182d25fb54d8a1
Reshma Abraham <reshmamabraham@gmail.com>
2020-03-19 20:48:23 +1100

    adding some echo comments

--- auto-script.sh
+++ auto-script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 while true; do
   python SensorApp.py
+  echo SensorApp-Client is running
   sleep 5
-  python SensorApp-Server.py
+  python SensorApp.py
+  echo SensorApp-Server is running
   sleep 5
 done
