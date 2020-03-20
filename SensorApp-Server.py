dd095bcb8bfb0f892329e2f699981a3ffcb59461
Reshma-M-A <31645577+Reshma-M-A@users.noreply.github.com>
2020-03-20 17:27:13 +1100

    Update SensorApp-Server.py

--- SensorApp-Server.py
+++ SensorApp-Server.py
@@ -1,77 +1,54 @@
-from __future__ import print_function # WalabotAPI works on both Python 2 an 3.
+from __future__ import print_function
 from sys import platform
 from os import system
 from imp import load_source
 from os.path import join
 import json
-from threading import Thread
-import time
 from socket import *
 
 if platform == 'win32':
-	modulePath = join('C:/', 'Program Files', 'Walabot', 'WalabotSDK',
-		'python', 'WalabotAPI.py')
+    modulePath = join('C:/', 'Program Files', 'Walabot', 'WalabotSDK',
+    'python', 'WalabotAPI.py')
 elif platform.startswith('linux'):
-    modulePath = join('/usr', 'share', 'walabot', 'python', 'WalabotAPI.py')
-
-walabot = load_source('WalabotAPI', modulePath)
-walabot.Init()
+    modulePath = join('/usr', 'share', 'walabot', 'python', 'WalabotAPI.py') 
 
 sock = socket(AF_INET, SOCK_DGRAM)
 sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
-def NetworkSendTargets(targets):
-    if targets:
-        data = {"targets": []}
-        for i, target in enumerate(targets):
-            data["targets"].append({
-                "targetID":i,
-                "x": target.xPosCm,
-                "y": target.yPosCm,
-                "z": target.zPosCm,
-                "a": target.amplitude
-            })
-        sock.sendto(json.dumps(data), ('255.255.255.255', 5455))
+wlbt = load_source('WalabotAPI', modulePath)
+wlbt.Init()
 
-def Sensor():
-    ArenaMinimumSize, ArenaMaximumSize, ArenaResolution = 30, 200, 3
-    ArenaMinimumDegrees, ArenaMaximumDegrees, ArenaDegreesResolution = -15, 15, 5
-    ArenaMinimumPhi, ArenaMaximumPhi, ArenaPhiResolution = -60, 60, 5
+def SensorApp():
+    minInCm, maxInCm, resInCm = 30, 200, 3
+    minIndegrees, maxIndegrees, resIndegrees = -15, 15, 5
+    minPhiInDegrees, maxPhiInDegrees, resPhiInDegrees = -60, 60, 5
     mtiMode = False
-
-    walabot.Initialize()
-    walabot.ConnectAny()
-
-    walabot.SetProfile(walabot.PROF_SENSOR)
-
-    walabot.SetAreanaR(ArenaMinimumSize, ArenaMaximumSize, ArenaResolution)
-    walabot.SetArenaTheta(ArenaMinimumDegrees, ArenaMaximumDegrees, ArenaDegreesResolution)
-    walabot.SetAreanaPhi(ArenaMinimumPhi, ArenaMaximumPhi, ArenaPhiResolution)
-
-    filterType = walabot.FILTER_TYPE_MTI if mtimode else walabot.FILTER_TYPE_NONE
-    walabot.SetDynamicImageFilter(filterType)
-    
-    walabot.Start()
-    
+    wlbt.Initialize()
+    wlbt.ConnectAny()
+    wlbt.SetProfile(wlbt.PROF_SENSOR)
+    wlbt.SetArenaR(minInCm, maxInCm, resInCm)
+    wlbt.SetArenaTheta(minIndegrees, maxIndegrees, resIndegrees)
+    wlbt.SetArenaPhi(minPhiInDegrees, maxPhiInDegrees, resPhiInDegrees)
+    filterType = wlbt.FILTER_TYPE_MTI if mtiMode else wlbt.FILTER_TYPE_NONE
+    wlbt.SetDynamicImageFilter(filterType)
+    wlbt.Start()
     if not mtiMode:
-        input("Calibration Required. Press any key once area is clear...")
-        walabot.StartCalibration()
-        while walabot.GetStatus()[0] == walabot.STATUS_CALIBRATING:
-            print("Calibrating...")
-            walabot.Trigger()
-    
+        wlbt.StartCalibration()
+	print("Calibrating")
+    while wlbt.GetStatus()[0] == wlbt.STATUS_CALIBRATING:
+        wlbt.Trigger()
     while True:
-            appStatus, calibrationProcess = walabot.GetStatus()
-            walabot.Trigger()
-            targets = walabot.GetSensorTargets()
-            rasterImage, _, _, sliceDepth, power = walabot.GetRawImageSlize()
-            NetworkSendTargets(targets)
-    
-    walabot.Stop()
-    walabot.Disconnect()
-    walabot.Clean()
-    print("Terminated Successfully.")
+        appStatus, calibrationProcess = wlbt.GetStatus()
+        wlbt.Trigger()
+        targets = wlbt.GetRawImageSlice()
+        rasterImage, _, _, sliceDepth, power = wlbt.GetRawImageSlice()
+        sock.sendto(json.dumps(targets),('255.255.255.255',5455))
+        print("Sent Data.")
+    wlbt.Stop()
+    wlbt.Disconnect()
+    wlbt.Clean()
+    print('Terminate successfully')
 
 if __name__ == '__main__':
-    Sensor()
+    SensorApp()
