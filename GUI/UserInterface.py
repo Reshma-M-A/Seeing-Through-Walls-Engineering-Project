cd818f5e199f87b0d38ab292ae58ba2c6bfc1e11
Reshma Abraham <reshmamabraham@gmail.com>
2020-04-14 21:09:04 +1000

    using grid system

--- GUI/UserInterface.py
+++ GUI/UserInterface.py
@@ -3,8 +3,11 @@ from Tkinter import *
 
 root = Tk()
 # Creating a Label widget
-myLabel = Label(root, text="Hello World!")
+myLabel1 = Label(root, text="Hello World!")
+myLabel2 = Label(root, text="My name is John Wick")
 # Shoving it onto the screen
-myLabel.pack()
+
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=1)
 
 root.mainloop()
